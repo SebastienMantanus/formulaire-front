570f2f5f74373d011db96a77c0bb8a912b605465
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2022-12-10 11:02:10 +0100

    correction form name

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("document loaded");
 
-  document
-    .querySelector("#contactForm")
-    .addEventListener("submit", async (e) => {
-      e.preventDefault();
+  document.querySelector("#MailForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-      const data = {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
-      };
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
 
-      console.log(data);
+    console.log(data);
 
-      const response = await axios.post("http://localhost:3000/form", data);
-      console.log(response);
-    });
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
+  });
 });
